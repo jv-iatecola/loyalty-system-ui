e194c5f3da900fd2d54a3839b904559888e4e0dc
jv-iatecola <jv.iatecola10@gmail.com>
2024-07-27 18:58:14 -0300

    add logout button

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,3 +2,14 @@ const localStorageContent = localStorage.getItem("Token")
 if (!localStorageContent){
     location.assign("/create-account.html")
 }
+
+
+const logoutButtonElement = document.createElement("button")
+logoutButtonElement.textContent = "Logout"
+
+document.body.prepend(logoutButtonElement)
+
+logoutButtonElement.onclick = ()=>{
+    localStorage.clear()
+    location.assign("/create-account.html")
+}
